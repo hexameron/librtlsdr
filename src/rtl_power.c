hexameron/librtlsdr

commit 6ad1b618fcdc9f21b341f3d73d4117e7b33abbc9

rtl_power: Fix overflow to negative number.
Causes "Not A Number" when taking log of power spectrum.
Unsigned accumulator allows more headroom, and less severe consequences.
Upstream uses 64bit integers to ensure there will be no overflow.

src/rtl_power.c
@@ -92,7 +92,7 @@ struct tuning_state
 	int freq;
 	int rate;
 	int bin_e;
-	long *avg;  /* length == 2^bin_e */
+	unsigned long *avg;  /* length == 2^bin_e */
 	int samples;
 	int downsample;
 	int downsample_passes;  /* for the recursive filter */
@@ -413,13 +413,13 @@ void rms_power(struct tuning_state *ts)
 	}
 	/* correct for dc offset in squares */
 	dc = (double)t / (double)buf_len;
-	err = t * 2 * dc - dc * dc * buf_len;
+	err = dc * (double)t;
 	p -= (long)round(err);
 
 	if (!peak_hold) {
-		ts->avg[0] += p;
+		ts->avg[0] += (unsigned long)p;
 	} else {
-		ts->avg[0] = MAX(ts->avg[0], p);
+		ts->avg[0] = MAX(ts->avg[0], (unsigned long)p);
 	}
 	ts->samples += 1;
 }
@@ -501,7 +501,7 @@ void frequency_range(char *arg, double crop)
 		ts->crop = crop;
 		ts->downsample = downsample;
 		ts->downsample_passes = downsample_passes;
-		ts->avg = (long*)malloc((1<<bin_e) * sizeof(long));
+		ts->avg = (unsigned long*)malloc((1<<bin_e) * sizeof(long));
 		if (!ts->avg) {
 			fprintf(stderr, "Error: malloc.\n");
 			exit(1);
@@ -700,7 +700,7 @@ void scanner(void)
 				}
 			} else {
 				for (j=0; j<bin_len; j++) {
-					ts->avg[j] = MAX(real_conj(fft_buf[offset+j*2], fft_buf[offset+j*2+1]), ts->avg[j]);
+					ts->avg[j] = MAX((unsigned long)real_conj(fft_buf[offset+j*2], fft_buf[offset+j*2+1]), ts->avg[j]);
 				}
 			}
 			ts->samples += ds;

commit aea2a5352da712a043c5c45c9112999ce005b587

rtl_power: Avoid clipping.
 Multiple Downsampling can result in 64k oversampling. Scale down
  samples by 4 bits to leave headroom in a 32bit accumulator.

 Fixes: 6ad1b618fcdc

src/rtl_power.c
@@ -107,8 +107,8 @@ struct tuning_state
 	//pthread_mutex_t buf_mutex;
 };
 
-/* 3000 is enough for 3GHz b/w worst case */
-#define MAX_TUNES	3000
+/* 50 is enough for 100 MHz b/w */
+#define MAX_TUNES	50
 struct tuning_state tunes[MAX_TUNES];
 int tune_count = 0;
 
@@ -507,7 +507,7 @@ void frequency_range(char *arg, double crop)
 			exit(1);
 		}
 		for (j=0; j<(1<<bin_e); j++) {
-			ts->avg[j] = 0L;
+			ts->avg[j] = 1L;
 		}
 		ts->buf8 = (uint8_t*)malloc(buf_len * sizeof(uint8_t));
 		if (!ts->buf8) {
@@ -625,7 +625,9 @@ void downsample_iq(int16_t *data, int length)
 long real_conj(int16_t real, int16_t imag)
 /* real(n * conj(n)) */
 {
-	return ((long)real*(long)real + (long)imag*(long)imag);
+	// scales 16 bit to 32 bit, so we can gain 4 bits of headroom
+	// for the accumulator and not lose any precision.
+	return ((long)real*(long)real + (long)imag*(long)imag) >> 4;
 }
 
 void scanner(void)
@@ -712,7 +714,7 @@ void csv_dbm(struct tuning_state *ts)
 {
 	int i, len, ds, i1, i2, bw2, bin_count;
 	long tmp;
-	double dbm;
+	double dbm, dbm_rescale;
 	len = 1 << ts->bin_e;
 	ds = ts->downsample;
 	/* fix FFT stuff quirks */
@@ -734,21 +736,24 @@ void csv_dbm(struct tuning_state *ts)
 	// something seems off with the dbm math
 	i1 = 0 + (int)((double)len * ts->crop * 0.5);
 	i2 = (len-1) - (int)((double)len * ts->crop * 0.5);
+        dbm_rescale = 16.0 / (double)ts->rate / (double)ts->samples;
 	for (i=i1; i<=i2; i++) {
 		dbm  = (double)ts->avg[i];
-		dbm /= (double)ts->rate;
-		dbm /= (double)ts->samples;
+		dbm *= dbm_rescale;
 		dbm  = 10 * log10(dbm);
 		fprintf(file, "%.2f, ", dbm);
 	}
-	dbm = (double)ts->avg[i2] / ((double)ts->rate * (double)ts->samples);
+	dbm = (double)ts->avg[i2] * dbm_rescale;
 	if (ts->bin_e == 0) {
-		dbm = ((double)ts->avg[0] / \
-		((double)ts->rate * (double)ts->samples));}
+		dbm = ((double)ts->avg[0] * dbm_rescale);}
 	dbm  = 10 * log10(dbm);
 	fprintf(file, "%.2f\n", dbm);
+
+	// Dynamic range is shifted up 4 bits: -84dB to +12dB
+	// We can hit "zero" at the edges with zero gain
+	// Max gain gives a noise floor around -30dB
 	for (i=0; i<len; i++) {
-		ts->avg[i] = 0L;
+		ts->avg[i] = 1L;
 	}
 	ts->samples = 0;
 }